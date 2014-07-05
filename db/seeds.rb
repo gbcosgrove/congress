27d6903b518b7ed54d0c546016e7b83974d45c30
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-05 12:29:08 -0500

    Add db seed method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'sunlight'
+require 'influence_explorer'
+
+pols = Sunlight::Legislator.all_where(in_office: 'true')
+
+pols.each do |pol|
+    hash = {title: pol.title, firstname: pol.firstname,
+            nickname: pol.nickname, last_name: pol.lastname,
+            name_suffix: pol.name_suffix, party: pol.party, state: pol.state,
+            district: pol.district, in_office: pol.in_office, gender: pol.gender, birthdate: pol.birthdate,
+            senate_class: pol.senate_class, bioguide_id: pol.bioguide_id, votesmart_id: pol.votesmart_id,
+            govtrack_id: pol.govtrack_id, crp_id: pol.crp_id}
+      Politician.create(hash)
+end
